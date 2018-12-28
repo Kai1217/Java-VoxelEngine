984a3434385f8af718c6ab217775cfa7902accce
Kai Matolat <kaimatolat@hotmail.com>
2018-12-29 01:30:24 +1000

    Added user input. Current output is in the console.

--- src/engine/io/Window.java
+++ src/engine/io/Window.java
@@ -1,13 +1,18 @@
 package engine.io;
 
+import org.lwjgl.BufferUtils;
 import org.lwjgl.glfw.GLFW;
 import org.lwjgl.glfw.GLFWVidMode;
+
+import java.nio.DoubleBuffer;
 
 public class Window
 {
     private int width, height;
     private String title;
     private long window;
+    private boolean[] keys = new boolean[GLFW.GLFW_KEY_LAST];
+    private boolean[] mouseButtons = new boolean[GLFW.GLFW_MOUSE_BUTTON_LAST];
 
     public Window(int width, int height, String title)
     {
@@ -46,11 +51,57 @@ public class Window
 
     public void update()
     {
+        for (int i = 0; i < GLFW.GLFW_KEY_LAST; i++) keys[i] = isKeyDown(i);
+        for (int i = 0; i < GLFW.GLFW_MOUSE_BUTTON_LAST; i++) mouseButtons[i] = isMouseDown(i);
         GLFW.glfwPollEvents();
     }
 
     public void swapBuffers()
     {
         GLFW.glfwSwapBuffers(window);
+    }
+
+    public boolean isKeyDown(int keyCode)
+    {
+        return GLFW.glfwGetKey(window, keyCode) == 1;
+    }
+
+    public boolean isMouseDown(int mouseKey)
+    {
+        return GLFW.glfwGetMouseButton(window, mouseKey) == 1;
+    }
+
+    public boolean isKeyPressed(int keyCode)
+    {
+        return isKeyDown(keyCode) && !keys[keyCode];
+    }
+
+    public boolean isKeyReleased(int keyCode)
+    {
+        return !isKeyDown(keyCode) && keys[keyCode];
+    }
+
+    public boolean isMousePressed(int mouseButton)
+    {
+        return isMouseDown(mouseButton) && !mouseButtons[mouseButton];
+    }
+
+    public boolean isMouseReleased(int mouseButton)
+    {
+        return !isMouseDown(mouseButton) && mouseButtons[mouseButton];
+    }
+
+    public double getMouseX()
+    {
+        DoubleBuffer buffer = BufferUtils.createDoubleBuffer(1);
+        GLFW.glfwGetCursorPos(window, buffer, null);
+        return buffer.get(0);
+    }
+
+    public double getMouseY()
+    {
+        DoubleBuffer buffer = BufferUtils.createDoubleBuffer(1);
+        GLFW.glfwGetCursorPos(window, null, buffer);
+        return buffer.get(0);
     }
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,18 +1,22 @@
 package main;
 
+import org.lwjgl.glfw.GLFW;
 import engine.io.Window;
 
 public class Main
 {
     public static void main(String[] args)
     {
-        Window window = new Window(800, 600, "LWJGL");
+        Window window = new Window(800, 600, "JavaVoxelEngine");
         window.create();
 
         while (!window.closed())
         {
             window.update();
-            System.out.println("Hello");
+            if (window.isKeyPressed(GLFW.GLFW_KEY_A)) System.out.println("A is pressed");
+            if (window.isKeyReleased(GLFW.GLFW_KEY_A)) System.out.println("A is released");
+            if (window.isMousePressed(GLFW.GLFW_MOUSE_BUTTON_LEFT)) System.out.println("Left mouse button is pressed at: " + window.getMouseX() + ", " + window.getMouseY() + "");
+            if (window.isMouseReleased(GLFW.GLFW_MOUSE_BUTTON_LEFT)) System.out.println("Left MB is released");
             window.swapBuffers();
         }
     }
